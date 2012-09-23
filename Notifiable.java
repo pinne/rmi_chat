0618ba60f6282f6ed39e4ec4238b04e4fc055e8c
Simon Kers <skers@kth.se>
2012-09-23 17:20:25 +0200

    Adding header comments

--- Notifiable.java
+++ Notifiable.java
@@ -1,3 +1,11 @@
+/**
+ * Communication Systems, HI1032
+ * Lab assignment 4A - Remote Method Invocation
+ *
+ * Simon Kers          skers@kth.se
+ * David Wikmans       wikmans@kth.se
+ *                                 KTH STH 2012
+ */
 import java.rmi.*;
 
 public interface Notifiable extends Remote {
